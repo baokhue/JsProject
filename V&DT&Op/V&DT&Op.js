3d0ba7dc81643c83f4fcfe42c7ccc5b1ba9b91c7
baokhue <khue49945@donga.edu.vn>
2022-11-21 20:47:12 +0700

    Add D2/Pract2.html file

---
+++ V&DT&Op/V&DT&Op.js
@@ -0,0 +1,17 @@
+/*
+function Plus() {
+    let result = +document.getElementById('No1').value + +document.getElementById('No2').value;
+    document.getElementById('Output').innerHTML = result;
+}
+function Minus() {
+    let result1 = +document.getElementById('No1').value - +document.getElementById('No2').value;
+    document.getElementById('Output').innerHTML = result1;
+}
+function Multiple() {
+    let result2 = +document.getElementById('No1').value * +document.getElementById('No2').value;
+    document.getElementById('Output').innerHTML = result2;
+}
+function Divide() {
+    let result3 = +document.getElementById('No1').value  / +document.getElementById('No2').value;
+    document.getElementById('Output').innerHTML = result3;
+}*/
